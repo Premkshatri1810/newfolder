910d7288455bf82d33a79fbc701f7884dc8e2ba4
user <premkshatri1810@gmail.com>
2018-02-10 19:03:16 +0530

    Added full project

---
+++ src/main/java/com/newfolder/NewfolderApplication.java
@@ -0,0 +1,12 @@
+package com.newfolder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewfolderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewfolderApplication.class, args);
+	}
+}
